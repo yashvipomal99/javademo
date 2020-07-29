4a209d180a3a43f14ed06b9c84349c2a0dc2c524
Yashvi Pomal <yashvipomal99@gmail.com>
2020-07-29 12:15:47 +0530

    Welcome to github

---
+++ mavenproject1/src/main/java/com/maven/mavenproject1/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.maven.mavenproject1;
+
+/**
+ *
+ * @author Yashvi Pomal
+ */
+public class App {
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Welcome to github!" );
+    }
+    
+}
